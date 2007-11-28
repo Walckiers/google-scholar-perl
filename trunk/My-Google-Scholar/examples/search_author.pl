658f47357308e8919a7309ef621dd55d6e2a7a0c
jjmerelo <jjmerelo@08a38ba2-ee3f-0410-9e2d-d95b085d0761>
2007-11-28 19:05:49 +0000

    New example

---
+++ trunk/My-Google-Scholar/examples/search_author.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib qw( lib ../lib );
+
+use My::Google::Scholar;
+
+my $scholar = My::Google::Scholar->new( { num => 100,
+					  as_subj => 'eng' });
+
+my $papers = $scholar->search_author( 'Koza, John' ); # Returns My::Google::Scholar::Paper
+
+for my $p (@$papers ) {
+  print "Title \"", $p->title(), "\", cited by ", $p->cited_by(), " \n";
+}
